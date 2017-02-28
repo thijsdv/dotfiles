3e8b966a13546a8d7067b119e759e459699e69cc
Thijs Devalckeneer <thijs.devalckeneer@altran.com>
2017-02-28 07:57:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,24 +9,30 @@ clear
 # Install zsh if needed
 # ---------------------------------------------------
 if ! location="$(type -p "zsh")" || [ -z "$location" ]; then
+    echo Installing zsh
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 # ---------------------------------------------------
 # Powerlevel9k zsh theme
 # ---------------------------------------------------
-echo Installing Powerlevel9k zsh theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k &>/dev/null
+if ! location="$(find ~/.oh-my-zsh --name "powerlevel9k")" || [ -z "$location" ]; then
+    echo Installing Powerlevel9k zsh theme
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k &>/dev/null
+fi
 
 # ---------------------------------------------------
 # Awesome Fonts
 # ---------------------------------------------------
-echo Installing Awesome Font
-git clone git@github.com:gabrielelana/awesome-terminal-fonts.git
-cp awesome-terminal-fonts/build/* ~/.fonts/
-sudo fc-cache -vf ~/.fonts/
-ln -sf 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-rm -rf awesome-terminal-fonts
+if ! location="$(find ~/.fonts --name "*awesome*")" || [ -z "$location" ]; then
+    echo Installing Awesome Font
+    git clone git@github.com:gabrielelana/awesome-terminal-fonts.git
+    cp awesome-terminal-fonts/build/* ~/.fonts/
+    cp awesome-terminal-fonts/config/* ~/.fonts/
+    sudo fc-cache -vf ~/.fonts/
+    ln -sf ~/.fonts/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+    rm -rf awesome-terminal-fonts
+fi
 
 # ---------------------------------------------------
 # Symlink zshrc
@@ -37,6 +43,9 @@ ln -sf ~/dotfiles/zshrc ~/.zshrc
 # ---------------------------------------------------
 # Symlink vimrc and install plugins
 # ---------------------------------------------------
+if ! location="$(find ~/.vim --name "vundle")" || [ -z "$location" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
@@ -44,3 +53,9 @@ vim +PluginInstall +qall
 # Symlink i3 config
 # ---------------------------------------------------
 ln -sf ~/dotfiles/i3config ~/.config/i3/config
+
+
+# ---------------------------------------------------
+# Set zsh as default sh
+# ---------------------------------------------------
+chsh -s $(which zsh)
