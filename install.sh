b87a1b35597b4498730acc28a6f8a253ac02b520
Thijs Devalckeneer <thijs.devalckeneer@altran.com>
2017-02-28 07:56:06 +0100

    Add i3 config

--- install.sh
+++ install.sh
@@ -43,4 +43,4 @@ vim +PluginInstall +qall
 # ---------------------------------------------------
 # Symlink i3 config
 # ---------------------------------------------------
-# ln -sf ~/dotfiles/i3/.config ~/.config/i3/config
+ln -sf ~/dotfiles/i3config ~/.config/i3/config
