dc21aed2b899ee8aa79f3e3ce46e2c05b523aad7
thijsdv <thijs.devalckeneer@altran.com>
2018-03-15 12:10:57 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,38 +1,8 @@
 #!/usr/bin/env sh
 
-# This script will only work if vim, zsh and oh-my-zsh are installed
+# This script will symlink all the config files
 
 clear
-
-
-# ---------------------------------------------------
-# Install zsh if needed
-# ---------------------------------------------------
-if ! location="$(type -p "zsh")" || [ -z "$location" ]; then
-    echo Installing zsh
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-# ---------------------------------------------------
-# Powerlevel9k zsh theme
-# ---------------------------------------------------
-if ! location="$(find ~/.oh-my-zsh --name "powerlevel9k")" || [ -z "$location" ]; then
-    echo Installing Powerlevel9k zsh theme
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k &>/dev/null
-fi
-
-# ---------------------------------------------------
-# Awesome Fonts
-# ---------------------------------------------------
-if ! location="$(find ~/.fonts --name "*awesome*")" || [ -z "$location" ]; then
-    echo Installing Awesome Font
-    git clone git@github.com:gabrielelana/awesome-terminal-fonts.git
-    cp awesome-terminal-fonts/build/* ~/.fonts/
-    cp awesome-terminal-fonts/config/* ~/.fonts/
-    sudo fc-cache -vf ~/.fonts/
-    ln -sf ~/.fonts/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-    rm -rf awesome-terminal-fonts
-fi
 
 # ---------------------------------------------------
 # Symlink zshrc
@@ -54,8 +24,7 @@ vim +PluginInstall +qall
 # ---------------------------------------------------
 ln -sf ~/dotfiles/i3config ~/.config/i3/config
 
-
 # ---------------------------------------------------
-# Set zsh as default sh
+# Symlink xonsh config
 # ---------------------------------------------------
-chsh -s $(which zsh)
+ln -sf ~/dotfiles/xonsh_config.json ~/.config/xonsh/config.json
